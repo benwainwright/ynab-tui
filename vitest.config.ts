c7fe047aa45c603dcd7c46677b4872f785882641
Ben Wainwright <bwainwright28@gmail.com>
2024-09-21 19:15:30 +0100

    Add CI

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,19 +2,21 @@ import { defineConfig } from 'vitest/config'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    coverage: {
-      include: ['src/**/*.ts'],
-      exclude: ['src/test-support/*.ts'],
-      thresholds: {
-        functions: 100,
-        lines: 100,
-        branches: 100,
-        statements: 100,
-      },
+    plugins: [tsconfigPaths()],
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        coverage: {
+            enabled: true,
+            reporter: ['html'],
+            include: ['src/**/*.ts', 'src/**/*.tsx'],
+            exclude: ['src/test-support/*.ts'],
+            thresholds: {
+                functions: 100,
+                lines: 100,
+                branches: 100,
+                statements: 100,
+            },
+        },
     },
-  },
 })
